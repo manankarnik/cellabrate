19f529be7fb8fdbd3a8ca8aa46cd280ab0fa8fb8
Manan Karnik <manankarnik@gmail.com>
2025-03-30 01:14:18 +0530

    Clear grid and resize screen

--- main.go
+++ main.go
@@ -24,13 +24,9 @@ func main() {
 	}
 	defer screen.Fini()
 
+	go poll(screen)
+
 	width, height := screen.Size()
-	if width == 0 || height == 0 {
-		return
-	}
-
-	go poll(screen, width, height)
-
 	for col := range width {
 		grid = append(grid, []int{})
 		for range height * 2 {
@@ -49,102 +45,22 @@ func main() {
 		for {
 			if simulate {
 				update()
-				time.Sleep(time.Millisecond * 200)
+				time.Sleep(time.Millisecond * 50)
 			}
-			draw(screen, width, height)
+			draw(screen)
 		}
 	}()
 
 	<-quit
 }
 
-func getNeighbors(row, col int) int {
-	neighbors := 0
-	if row > 0 {
-		neighbors += grid[row-1][col]
-		if col > 0 {
-			neighbors += grid[row-1][col-1]
-		}
-		if col < len(grid[0])-1 {
-			neighbors += grid[row-1][col+1]
-		}
-	}
-	if row < len(grid)-1 {
-		neighbors += grid[row+1][col]
-		if col > 0 {
-			neighbors += grid[row+1][col-1]
-		}
-		if col < len(grid[0])-1 {
-			neighbors += grid[row+1][col+1]
-		}
-	}
-	if col > 0 {
-		neighbors += grid[row][col-1]
-	}
-	if col < len(grid[0])-1 {
-		neighbors += grid[row][col+1]
-	}
-	return neighbors
-}
-
-func poll(screen tcell.Screen, width, height int) {
-	for {
-		event := screen.PollEvent()
-		switch event := event.(type) {
-		case *tcell.EventKey:
-			switch event.Key() {
-			case tcell.KeyEscape:
-				close(quit)
-				return
-			case tcell.KeyUp:
-				if cursor[1] > 0 {
-					cursor[1] -= 1
-				}
-			case tcell.KeyLeft:
-				if cursor[0] > 0 {
-					cursor[0] -= 1
-				}
-			case tcell.KeyDown:
-				if cursor[1] < height*2-1 {
-					cursor[1] += 1
-				}
-			case tcell.KeyRight:
-				if cursor[0] < width-1 {
-					cursor[0] += 1
-				}
-			}
-			switch event.Rune() {
-			case 'q':
-				close(quit)
-				return
-			case 'w':
-				if cursor[1] > 0 {
-					cursor[1] -= 1
-				}
-			case 'a':
-				if cursor[0] > 0 {
-					cursor[0] -= 1
-				}
-			case 's':
-				if cursor[1] < height*2-1 {
-					cursor[1] += 1
-				}
-			case 'd':
-				if cursor[0] < width-1 {
-					cursor[0] += 1
-				}
-			case ' ':
-				grid[cursor[0]][cursor[1]] ^= 1
-			case 'f':
-				simulate = !simulate
-			}
-		}
-	}
-}
-
-func draw(screen tcell.Screen, width, height int) {
+func draw(screen tcell.Screen) {
+	width, height := screen.Size()
 	const ch = '▄'
 	style := tcell.StyleDefault
+	if len(grid) < width && len(grid[0]) < height*2 {
+		resizeGrid(width, height)
+	}
 	for col := range width {
 		for row := range height {
 			if grid[col][row*2] == 0 {
@@ -191,6 +107,131 @@ func update() {
 				if neighbors[row][col] == 3 {
 					grid[row][col] = 1
 				}
+			}
+		}
+	}
+}
+
+func getNeighbors(row, col int) int {
+	neighbors := 0
+	if row > 0 {
+		neighbors += grid[row-1][col]
+		if col > 0 {
+			neighbors += grid[row-1][col-1]
+		}
+		if col < len(grid[0])-1 {
+			neighbors += grid[row-1][col+1]
+		}
+	}
+	if row < len(grid)-1 {
+		neighbors += grid[row+1][col]
+		if col > 0 {
+			neighbors += grid[row+1][col-1]
+		}
+		if col < len(grid[0])-1 {
+			neighbors += grid[row+1][col+1]
+		}
+	}
+	if col > 0 {
+		neighbors += grid[row][col-1]
+	}
+	if col < len(grid[0])-1 {
+		neighbors += grid[row][col+1]
+	}
+	return neighbors
+}
+
+func clearGrid(screen tcell.Screen) {
+	width, height := screen.Size()
+	for col := range width {
+		for row := range height * 2 {
+			if grid[col][row] == 1 {
+				grid[col][row] = 0
+			}
+		}
+	}
+}
+
+func resizeGrid(width, height int) {
+	for {
+		row := []int{}
+		for range grid[0] {
+			row = append(row, 0)
+		}
+		grid = append(grid, row)
+		if len(grid) == width {
+			break
+		}
+	}
+	for i := range grid {
+		for {
+			grid[i] = append(grid[i], 0)
+			if len(grid[i]) == height*2 {
+				break
+			}
+		}
+	}
+}
+
+func poll(screen tcell.Screen) {
+	for {
+		width, height := screen.Size()
+		event := screen.PollEvent()
+		switch event := event.(type) {
+		case *tcell.EventKey:
+			switch event.Key() {
+			case tcell.KeyEscape:
+				close(quit)
+				return
+			case tcell.KeyUp:
+				if cursor[1] > 0 {
+					cursor[1] -= 1
+				}
+			case tcell.KeyLeft:
+				if cursor[0] > 0 {
+					cursor[0] -= 1
+				}
+			case tcell.KeyDown:
+				if cursor[1] < height*2-1 {
+					cursor[1] += 1
+				}
+			case tcell.KeyRight:
+				if cursor[0] < width-1 {
+					cursor[0] += 1
+				}
+			}
+			switch event.Rune() {
+			case 'q':
+				close(quit)
+				return
+			case 'w':
+			case 'k':
+				if cursor[1] > 0 {
+					cursor[1] -= 1
+				}
+			case 'a':
+			case 'h':
+				if cursor[0] > 0 {
+					cursor[0] -= 1
+				}
+			case 's':
+			case 'j':
+				if cursor[1] < height*2-1 {
+					cursor[1] += 1
+				}
+			case 'd':
+			case 'l':
+				if cursor[0] < width-1 {
+					cursor[0] += 1
+				}
+			case ' ':
+				if !simulate {
+					grid[cursor[0]][cursor[1]] ^= 1
+				}
+			case 'f':
+				simulate = !simulate
+			case 'c':
+				clearGrid(screen)
 			}
 		}
 	}
